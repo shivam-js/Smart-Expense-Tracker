5bc68c5384e9b29a51a6401db02503a3b6f36efb
shivam-js <prajapati2shivam@gmail.com>
2026-05-06 23:29:36 +0530

    Fixed MongoDB connection and API configuration

--- server/server.js
+++ server/server.js
@@ -1,9 +1,37 @@
+const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://smart-expense-tracker-kcyv.onrender.com"
-  ],
-  credentials: true
-}));
+const authRoutes = require("./routes/authRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://smart-expense-tracker-theta-coral.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/expenses", expenseRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Expense Tracker API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
